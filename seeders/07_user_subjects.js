08202dea76fc60807e5e30af297ef8b3948509e5
rohanjoby-ws <rohan.joby@wednesday.is>
2022-02-04 11:21:47 +0530

    refactor: update spacing

--- seeders/07_user_subjects.js
+++ seeders/07_user_subjects.js
@@ -2,7 +2,7 @@ module.exports = {
     up: queryInterface => {
         const range = require('lodash/range');
         const user_subjects = range(1, 6).map((value, index) => ({
-            user_id: index % 2 + 1,
+            user_id: (index % 2) + 1,
             subject_id: value
         }));
         return queryInterface.bulkInsert('user_subjects', user_subjects, {});
